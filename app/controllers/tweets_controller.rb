adfcb6aa5d3acab29254265362e42d726f09fad4
JFlah <thepacific@me.com>
2015-01-24 14:27:57 -0500

    saving

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,26 @@
 class TweetsController < ApplicationController
+
+	# Relates to new.html.erb file
+	# This controller relates to that view
+	# new creates instance of Tweet.rb (model)
 	def new
 		@tweet = Tweet.new
+	end
+
+	# Creates a "tweet"
+	# Renders 'new'.html.erb page
+	def create
+		@tweet = Tweet.create(tweet_params)
+		flash.now[:success] = "Tweet Created"
+		render 'new'
+	end
+
+	private
+
+	# Must sanitize what's incoming in tweets
+	# Tweet must be part of params hash,
+	# Then only permit content (avoids bad inputs)
+	def tweet_params
+		params.require(:tweet).permit(:content)
 	end
 end
